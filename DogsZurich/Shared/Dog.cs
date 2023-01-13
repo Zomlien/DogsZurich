11745b8d6ae978862c0b6f3ef872b5079174c949
laurafrei <117374962+laurafrei@users.noreply.github.com>
2023-01-13 15:04:50 +0100

    Add project files.

---
+++ DogsZurich/Shared/Dog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogsZurich.Shared
+{
+    internal class Dog
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Color Colors { get; set;}
+        public Breed Breed1 { get; set; }
+        public Breed Breed2 { get; set; }
+        public Breedstatus Breedstatuss { get; set; }
+        public Breedtype Breedtypee { get; set; }
+
+    }
+}
---
+++ DogsZurich/Shared/Dogowner.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogsZurich.Shared
+{
+    internal class Dogowner
+    {
+        public int Id { get; set; }
+        public Sex  Sexx { get; set; }
+        public Quartier Quartierr { get; set; }
+        public Kreis Kreiss { get; set; }
+        public Ageclass Ageclasss { get; set; }
+        public ICollection<Dog> Dogs { get; set; }
+    }
+}
